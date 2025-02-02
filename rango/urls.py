8edb0712e9ffd9d2e48f8671728cdd5d3c6d3af2
Rowan Scott <2719252s@student.gla.ac.uk>
2025-02-02 12:43:49 +0000

    fixed issue with chapter 9 excercises

--- rango/urls.py
+++ rango/urls.py
@@ -5,7 +5,7 @@ app_name = 'rango'
 LOGIN_URL = 'rango:login'
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
     path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
